d38e5e4c27f3424d781014f2a9bad01b9bff15a0
Ralph Ocdol <ralph.ocdol63@gmail.com>
2024-11-20 21:08:31 +0800

    Release/v0.7.0 (#11)
    * Bump versions & update dependencies
    
    * Fix edge case in weather widget
    
    If the temperature for the entire day is the same the range is 0, divide by 0 no bueno
    
    * Add Markets sort-by: change to sort by percent change
    
    * Remove unused class
    
    * Change environment variables behavior
    
    * Can now be inserted anywhere in the string
    * Can now insert multiple environment variables in a single string
    * Can now be escaped if prefixed with \
    
    * Extract group widget functionality into generic widget container
    
    * Add masonry layout functionality
    
    * Make the first full column the primary on mobile rather than the last
    
    * Change masonry options source
    
    * Add split-column widget
    
    * Add fallback-content-type property
    
    * Change simple icons provider
    
    * Improve formatting of request body
    
    * Fix edge case in popover
    
    * Include title in twitch channel data
    
    * Show twitch stream preview and title on avatar hover
    
    * Add alternative status code prameter
    
    * Use AltStatusCodes for HasFailing check
    
    * Fix simple icons URL
    
    * Remove unused class
    
    * Add custom API widget
    
    * Allow specifying headers in RSS feeds
    
    * fix: non-whole-hour timezones now correctly shown on Clock
    
    Timezones that used non-whole-hour definitions (ie. Australia/ACDT) not correctly shown on the Clock widget. This fix changes how the difference is calculated to account for both minutes, and hours.
    
    Due to the change, the text shown for relative timezone differences also had to be changed.
    
    * feat: added auto expand mobile page navigation
    
    * fix: indention
    
    * Give clock time min width to avoid inconsistent layout for 12 hour format
    
    * Simplify clock zone diff text and add title for more details
    
    * Add title prop to Market Name for full text of truncated names on hover
    
    * Allow changing max columns on split columns widget
    
    * Allow changing horizontal offset of popover
    
    * Update twitch channel stream preview popover
    
    * Allow using env variables in custom API URL
    
    * Add Dashboard Icons prefix support
    
    - defined new type IconSource and constants, presently supporting:
      - LocalFile
      - SimpleIcon
      - DashboardIcon
    - added new field to bookmarks and monitors to hold IconSource
    - adjusted IsSimpleIcon to get truthiness from IconSource field
    - generalised `toSimpleIconIfPrefixed` into `toRemoteResourceIconIfPrefixed`,
      adding support for `walkxcode`'s dashboard icons via CDN (svg)
    
    * Add DashboardIcon (PNG) Support
    - Refactored to make better naming sense
    - Added capability to select between the PNG and SVG offerings of DashboardIcons
    
    * Update icons implementation to use custom type
    
    * Update docs
    
    * Fix formatting
    
    * Update docs
    
    * Clear search input on submission
    
    * Add compact style for monitor widget
    
    * Make monitor widget compact style title size dynamic
    
    * Reduce status icon size
    
    * Reduce gap
    
    * Reduce margin
    
    * Restore title-url functionality on group titles
    
    * Fix middle mouse click
    
    * Prevent default on middle click
    
    * Don't focus new tab on middle click
    
    This is more in line with how the browser behaves on middle click
    
    ---------
    
    Co-authored-by: Svilen Markov <7613769+svilenmarkov@users.noreply.github.com>
    Co-authored-by: Eric Haughee <ehaughee@gmail.com>
    Co-authored-by: Cody Meadows <codyrm@gmail.com>
    Co-authored-by: Oliver Mitchell <oliver@polymerlabs.dev>
    Co-authored-by: 2Q2C0DE <2q2code@mxfer.com>
    Co-authored-by: Doc Em <doc@docslab.net>

--- internal/widget/monitor.go
+++ internal/widget/monitor.go
@@ -11,60 +11,25 @@ import (
 	"github.com/glanceapp/glance/internal/feed"
 )
 
-func statusCodeToText(status int, altStatusCodes []int) string {
-	if status == 200 || slices.Contains(altStatusCodes, status) {
-		return "OK"
-	}
-	if status == 404 {
-		return "Not Found"
-	}
-	if status == 403 {
-		return "Forbidden"
-	}
-	if status == 401 {
-		return "Unauthorized"
-	}
-	if status >= 400 {
-		return "Client Error"
-	}
-	if status >= 500 {
-		return "Server Error"
-	}
-
-	return strconv.Itoa(status)
-}
-
-func statusCodeToStyle(status int, altStatusCodes []int) string {
-	if status == 200 || slices.Contains(altStatusCodes, status) {
-		return "ok"
-	}
-
-	return "error"
-}
-
 type Monitor struct {
 	widgetBase `yaml:",inline"`
 	Sites      []struct {
 		*feed.SiteStatusRequest `yaml:",inline"`
 		Status                  *feed.SiteStatus `yaml:"-"`
 		Title                   string           `yaml:"title"`
-		IconUrl                 string           `yaml:"icon"`
-		IsSimpleIcon            bool             `yaml:"-"`
+		Icon                    CustomIcon       `yaml:"icon"`
 		SameTab                 bool             `yaml:"same-tab"`
 		StatusText              string           `yaml:"-"`
 		StatusStyle             string           `yaml:"-"`
 		AltStatusCodes          []int            `yaml:"alt-status-codes"`
 	} `yaml:"sites"`
-	ShowFailingOnly bool `yaml:"show-failing-only"`
-	HasFailing      bool `yaml:"-"`
+	Style           string `yaml:"style"`
+	ShowFailingOnly bool   `yaml:"show-failing-only"`
+	HasFailing      bool   `yaml:"-"`
 }
 
 func (widget *Monitor) Initialize() error {
 	widget.withTitle("Monitor").withCacheDuration(5 * time.Minute)
-
-	for i := range widget.Sites {
-		widget.Sites[i].IconUrl, widget.Sites[i].IsSimpleIcon = toSimpleIconIfPrefixed(widget.Sites[i].IconUrl)
-	}
 
 	return nil
 }
@@ -101,5 +66,40 @@ func (widget *Monitor) Update(ctx context.Context) {
 }
 
 func (widget *Monitor) Render() template.HTML {
+	if widget.Style == "compact" {
+		return widget.render(widget, assets.MonitorCompactTemplate)
+	}
+
 	return widget.render(widget, assets.MonitorTemplate)
+}
+
+func statusCodeToText(status int, altStatusCodes []int) string {
+	if status == 200 || slices.Contains(altStatusCodes, status) {
+		return "OK"
+	}
+	if status == 404 {
+		return "Not Found"
+	}
+	if status == 403 {
+		return "Forbidden"
+	}
+	if status == 401 {
+		return "Unauthorized"
+	}
+	if status >= 400 {
+		return "Client Error"
+	}
+	if status >= 500 {
+		return "Server Error"
+	}
+
+	return strconv.Itoa(status)
+}
+
+func statusCodeToStyle(status int, altStatusCodes []int) string {
+	if status == 200 || slices.Contains(altStatusCodes, status) {
+		return "ok"
+	}
+
+	return "error"
 }
